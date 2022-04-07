45073df9cdb96785e78beaa52047dfa4b0353c75
cdricms <36056008+cdricms@users.noreply.github.com>
2022-04-07 13:59:58 +0000

    Some new endpoints, but they have to be tested

--- frontend/src/redux/api/me.ts
+++ frontend/src/redux/api/me.ts
@@ -4,6 +4,15 @@ import { Pagination } from "../../Types/Interfaces/Pagination";
 import { User } from "../../Types/Interfaces/User";
 import { devroomApiAuth } from "./apiAuth";
 
+interface PostRequest {
+  body: string;
+}
+
+interface ArticleRequest {
+  body: string;
+  title: string;
+}
+
 const meApi = devroomApiAuth.injectEndpoints({
   endpoints: (builder) => ({
     getMe: builder.query<User, void>({
@@ -23,6 +32,41 @@ const meApi = devroomApiAuth.injectEndpoints({
     getMeArticles: builder.query<Pagination<Article>, number | void>({
       query: (limit) =>
         `user/articles/?limit${limit || 10}&offset=${limit || 10}`
+    }),
+    getMePostsLiked: builder.query<Pagination<Post>, number | void>({
+      query: (limit) =>
+        `user/posts/liked/?limit${limit || 10}&offset=${limit || 10}`
+    }),
+    getMeArticlesLiked: builder.query<Pagination<Article>, number | void>({
+      query: (limit) =>
+        `user/articles/liked/?limit${limit || 10}&offset=${limit || 10}`
+    }),
+    getMeComments: builder.query<Pagination<Post>, number | void>({
+      query: (limit) =>
+        `user/comments/?limit${limit || 10}&offset=${limit || 10}`
+    }),
+    getMeTLPosts: builder.query<Pagination<Post>, number | void>({
+      query: (limit) =>
+        `user/timeline/posts/?limit${limit || 10}&offset=${limit || 10}`
+    }),
+    getMeTLArticles: builder.query<Pagination<Article>, number | void>({
+      query: (limit) =>
+        `user/timeline/articles/?limit${limit || 10}&offset=${limit || 10}`
+    }),
+
+    newPost: builder.mutation<Post, PostRequest>({
+      query: (post) => ({
+        url: `user/posts/new/`,
+        method: "POST",
+        body: post
+      })
+    }),
+    newArticle: builder.mutation<Article, ArticleRequest>({
+      query: (article) => ({
+        url: `user/article/new/`,
+        method: "POST",
+        body: article
+      })
     })
   }),
   overrideExisting: false
@@ -38,5 +82,18 @@ export const {
   useGetMeArticlesQuery,
   useLazyGetMeArticlesQuery,
   useGetMePostsQuery,
-  useLazyGetMePostsQuery
+  useLazyGetMePostsQuery,
+  useGetMeCommentsQuery,
+  useLazyGetMeCommentsQuery,
+  useGetMePostsLikedQuery,
+  useLazyGetMePostsLikedQuery,
+  useGetMeArticlesLikedQuery,
+  useLazyGetMeArticlesLikedQuery,
+  useGetMeTLPostsQuery,
+  useLazyGetMeTLPostsQuery,
+  useGetMeTLArticlesQuery,
+  useLazyGetMeTLArticlesQuery,
+
+  useNewPostMutation,
+  useNewArticleMutation
 } = meApi;
